b4dd3d9da9fc6a59baa97893e30e2e1e84ae9eed
hpedamal <hpedamal@LIN24010063.corp.capgemini.com>
2022-02-26 10:46:17 +0530

    Added some Multi threading files

---
+++ src/multiThreading/CopyDataThread.java
@@ -0,0 +1,14 @@
+package multiThreading;
+
+import java.io.*;
+
+public class CopyDataThread extends Thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileInputStream fi;
+		FileOutputStream fo;
+		
+	}
+
+}
---
+++ src/multiThreading/Ex1.java
@@ -0,0 +1,10 @@
+package multiThreading;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
