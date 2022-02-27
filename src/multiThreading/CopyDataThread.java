917a0199f2b1052286307e7f12875c62674967f2
hpedamal <hpedamal@LIN24010063.corp.capgemini.com>
2022-02-27 23:02:13 +0530

    Added all files of multi threading

--- src/multiThreading/CopyDataThread.java
+++ src/multiThreading/CopyDataThread.java
@@ -3,12 +3,37 @@ package multiThreading;
 import java.io.*;
 
 public class CopyDataThread extends Thread {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FileInputStream fi;
-		FileOutputStream fo;
-		
+	FileReader fr;
+	FileWriter fw;
+	public CopyDataThread(FileReader fr, FileWriter fw) {
+		this.fr = fr;
+		this.fw = fw;
+	}
+	public void run() {
+		int num;
+		int cnt=0;
+		int i;
+		try {
+			  while((i=fr.read())!=-1)  
+			  {
+		          fw.write( ((char)i) ); 
+		          cnt++;
+		          if(cnt==10)
+		          {
+		        	  cnt=0;
+		        	  System.out.println("10 characters are copied");
+		        	  Thread.sleep(1000);
+		          }
+			  }
+		      fr.close();    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Task completed");
 	}
 
 }
